2fc255a4a5c8c6516100e64fb9f08a1266834be5
Kanat9494 <kkanat94@gmail.com>
2023-01-15 21:01:16 +0600

    Add project files.

---
+++ BiTrend/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BiTrend;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BiTrend/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BiTrend;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
